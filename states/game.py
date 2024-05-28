ee20a1cdb6f85c69f801c92beba0384207242ea8
Vannila <djb00z720@gmail.com>
2024-05-28 22:02:24 +0300

    State handling implemented

--- states/game.py
+++ states/game.py
@@ -19,15 +19,19 @@ class Game(State):
         self.assets_manager: AssetsManager = assets_manager
 
         # Attributes
-        self.screen_rect: pygame.Rect = pygame.display.get_surface().get_rect()
+        self.states = {"running": "gameover", "gameover": "running"}
+        self.state = "running"
+        self.rect_screen: pygame.Rect = pygame.display.get_surface().get_rect()
         self.pipe_gap: pygame.Vector2 = pygame.math.Vector2(100, 100)
 
+        # Sprites
         self.sprite_backgroud_day: pygame.Surface = self.assets_manager.sprites[
             "background-day"
         ]
         self.sprites_pipe: dict[str : pygame.Surface] = self.assets_manager.sprites[
             "pipes"
         ]
+        self.sprite_gameover: pygame.Surface = self.assets_manager.sprites["gameover"]
         sprites_bird: dict[str : dict[str : pygame.Surface]] = (
             self.assets_manager.sprites["birds"]
         )
@@ -35,6 +39,11 @@ class Game(State):
         sprites_digit: dict[str : pygame.Surface] = self.assets_manager.sprites[
             "digits"
         ]
+
+        # Sprite coordinates
+        self.rect_sprite_gameover: pygame.Rect = self.sprite_gameover.get_rect(
+            center=self.rect_screen.center
+        )
 
         # Groups
         self.pipe_group: pygame.sprite.Group = pygame.sprite.Group()
@@ -60,23 +69,20 @@ class Game(State):
         # Initalizing
         self.add_pipes_to_group()
 
-        # Test
-        self.game_state = "running"
-
     def add_pipes_to_group(self) -> None:
         sprite_pipe = self.sprites_pipe["green"]
         min_h = sprite_pipe.get_height()
-        max_h = self.screen_rect.h - sprite_pipe.get_height()
+        max_h = self.rect_screen.h - sprite_pipe.get_height()
 
         pipe_obj_1 = Pipe(
-            position=pygame.math.Vector2(self.screen_rect.w, randint(max_h, min_h)),
+            position=pygame.math.Vector2(self.rect_screen.w, randint(max_h, min_h)),
             velocity=pygame.math.Vector2(100, 0),
             sprite=sprite_pipe,
         )
 
         pipe_2_h = pipe_obj_1.rect.top - min_h - self.pipe_gap.y
         pipe_obj_2 = Pipe(
-            position=pygame.math.Vector2(self.screen_rect.w, pipe_2_h),
+            position=pygame.math.Vector2(self.rect_screen.w, pipe_2_h),
             velocity=pygame.math.Vector2(100, 0),
             sprite=pygame.transform.flip(
                 surface=sprite_pipe, flip_x=False, flip_y=True
@@ -90,40 +96,54 @@ class Game(State):
             if pipe.rect.right <= 0:
                 self.pipe_group.remove(pipe)
 
+    def next_state(self) -> None:
+        self.state = self.states[self.state]
+
+    def game_over(self) -> None:
+        self.next_state()
+
     def process_event(self, event: pygame.Event) -> None:
-        self.bird.process_event(event=event)
+        if self.state == "running":
+            self.bird.process_event(event=event)
 
     def update(self, delta_time: float) -> None:
-        # Updating groups
-        self.pipe_group.update(delta_time=delta_time)
-        self.platform_group.update(delta_time=delta_time)
-
-        # Add new pipe
-        grp_sprites = self.pipe_group.sprites()
-        if grp_sprites[-1].rect.right + self.pipe_gap.x < self.screen_rect.right:
-            self.add_pipes_to_group()
-
-        # Removes all the pipes of the screen
-        self.remove_pipes_from_group()
-
-        # Creating group for collision checking with a bird
-        self.collision_group = pygame.sprite.Group(
-            (self.platform_group, self.pipe_group)
-        )
-
-        # Updating the bird
         self.bird.update(delta_time=delta_time)
 
-        # Collision logic
-        if pygame.sprite.spritecollideany(sprite=self.bird, group=self.collision_group):
-            self.game_state = "Lost"
+        # Making sure the bird is above platform
+        for platform in self.platform_group:
+            if self.bird.rect.bottom > platform.rect.top:
+                self.bird.rect.bottom = platform.rect.top
 
-        # Add score
-        if self.bird.rect.centerx == self.pipe_group.sprites()[0].rect.centerx:
-            self.score_counter.add_to_counter()
+        if self.state == "running":
+            # Updating groups
+            self.pipe_group.update(delta_time=delta_time)
+            self.platform_group.update(delta_time=delta_time)
 
-        # Update score counter
-        self.score_counter.update()
+            # Add new pipe
+            grp_sprites = self.pipe_group.sprites()
+            if grp_sprites[-1].rect.right + self.pipe_gap.x < self.rect_screen.right:
+                self.add_pipes_to_group()
+
+            # Removes all the pipes of the screen
+            self.remove_pipes_from_group()
+
+            # Creating group for collision checking with a bird
+            self.collision_group = pygame.sprite.Group(
+                (self.platform_group, self.pipe_group)
+            )
+
+            # Collision logic
+            if pygame.sprite.spritecollideany(
+                sprite=self.bird, group=self.collision_group
+            ):
+                self.game_over()
+
+            # Add score
+            if self.bird.rect.centerx == self.pipe_group.sprites()[0].rect.centerx:
+                self.score_counter.add_to_counter()
+
+            # Update score counter
+            self.score_counter.update()
 
     def render(self, screen: pygame.Surface) -> None:
         screen.blit(self.sprite_backgroud_day, (0, 0))
@@ -132,3 +152,6 @@ class Game(State):
         self.score_counter.render(screen=screen)
         self.bird.render(screen=screen)
         self.platform_group.draw(surface=screen)
+
+        if self.state == "gameover":
+            screen.blit(self.sprite_gameover, self.rect_sprite_gameover)
